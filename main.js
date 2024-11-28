ea68ca408f8873b7df051d9975518c750dccd801
Diego Mateus <fcdiegomateus@gmail.com>
2024-11-28 13:40:22 -0500

    Cambio a ciclo For JavaScript

--- main.js
+++ main.js
@@ -7,8 +7,34 @@ function playSonidoPom (){
 
 document.querySelector('.tecla_pom').onclick = playSonidoPom; */
 
+
+function playSonido (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 
+
+
+for(let contador = 0; contador < ListaDeTeclas.length; contador++){
+    const tecla = ListaDeTeclas [contador];
+    const Instrumento = tecla.classList[1];
+
+    const idAudio = `#sonido_${Instrumento}`;
+    console.log(idAudio);
+
+    tecla.onclick = function(){
+        playSonido(idAudio);
+    }
+
+    tecla.onkeydown = function(){
+        tecla.classList.add('activa');
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('activa');
+    }
+}
 
 
 
